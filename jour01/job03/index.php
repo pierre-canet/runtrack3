1ad1602372bcae079c2046b1c5dfc520d591ca75
pierre-canet <pierre.canet@laplateforme.io>
2025-10-24 10:52:48 +0200

    Ajout du jour01/job03

---
+++ jour01/job03/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <link rel="stylesheet" href="index.css" />
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <p>Bonjour</p>
+</body>
+
+</html>
