312e559f0918869752c25f876b4691273f41e83c
pierre-canet <pierre.canet@laplateforme.io>
2025-10-24 09:33:55 +0200

    Ajout du jour01/job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <link rel="stylesheet" href="index.css" />
+    <script>
+        function popupBasique(page, popupBasique, ) {
+            window.open(page);
+        }
+    </script>
+</head>
+
+<body>
+    <p>Bonjour</p>
+    <a href="javascript:popupBasique('popup.html')">Ouverture popup basique</a>
+</body>
+
+</html>
