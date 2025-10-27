b73c7f1c3c5ba6d83d064208a6701f9dcbb02a83
pierre-canet <pierre.canet@laplateforme.io>
2025-10-27 16:32:51 +0100

    Ajout du jour01/job05

---
+++ jour01/job05/script.js
@@ -0,0 +1,15 @@
+function afficherjourssemaine(){
+    const jourssemaine = [
+        'Lundi',
+        'Mardi',
+        'Mercredi',
+        'Jeudi',
+        'Vendredi',
+        'Samedi',
+        'Dimanche'
+    ];
+    for(const element of jourssemaine){
+        console.log(element)
+    }
+}
+afficherjourssemaine()
