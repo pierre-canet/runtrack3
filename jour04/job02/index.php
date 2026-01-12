0cbbdb643374a1426a25d8e1e7111b1e16d30f6f
pierre-canet <pierre.canet@laplateforme.io>
2026-01-13 00:11:44 +0100

    Ajout du jour04/job02

---
+++ jour04/job02/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Job 02</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ jour04/job02/script.js
@@ -0,0 +1,13 @@
+function jsonValueKey(jsonString, key) {
+  const obj = JSON.parse(jsonString);
+  return obj[key];
+}
+const json = `{
+    "name": "La Plateforme_",
+    "address": "8 rue d'hozier",
+    "city": "Marseille",
+    "nb_staff": "11",
+    "creation": "2019"
+}`;
+
+console.log(jsonValueKey(json, "nb_staff"));
