7f904fcdecac2dc6f9f86997e8e59b1692fc4a7f
pierre-canet <pierre.canet@laplateforme.io>
2026-01-12 23:17:43 +0100

    Ajout du jour03/job03 (bases)

---
+++ jour03/job03/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Jeu du Taquin</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div id="board"></div>
+    <p id="message"></p>
+    <button id="restart" style="display:none;">Recommencer</button>
+    <script src="script.js"></script>
+</body>
+
+</html>
